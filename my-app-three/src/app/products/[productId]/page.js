b26d8fba3d99f1bdce9d9c34a0cd9e4448f3f3c2
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2025-05-12 17:24:25 +0600

    not found

--- my-app-three/src/app/products/[productId]/page.js
+++ my-app-three/src/app/products/[productId]/page.js
@@ -1,5 +1,10 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductItem({params}) {
-    const { productId } = await params
+    const { productId } = await params ?? ''
+    if(productId > 100){
+        notFound()
+    }
     return (
         <div>
             <h1> Welcome in Product Item: {productId} </h1>

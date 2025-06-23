ce5d259e620b22b607807799744c0c03c890097d
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2025-06-23 11:29:28 +0600

    learning next

--- my-app-three/src/app/products/[productId]/[reviewId]/page.js
+++ my-app-three/src/app/products/[productId]/[reviewId]/page.js
@@ -1,10 +1,15 @@
+import { redirect } from 'next/navigation'
+
 export default async function ProductReviewItem({params}) {
     const randomBit = Math.floor(Math.random() * 2);
 
-    if(randomBit === 0){
-        throw new Error('Random error')
-    }
+    // if(randomBit === 0){
+    //     throw new Error('Random error')
+    // }
     const { productId, reviewId } = await params
+    if(reviewId > 100){
+        redirect('/')
+    }
     return (
         <div>
             <h1> Welcome in Product Item: {productId} and review id {reviewId}</h1>

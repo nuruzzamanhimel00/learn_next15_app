2cf8728724ee6a215dce58254b6a4908f5b92d5a
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2025-09-28 21:36:39 +0600

    done

--- my-app-four/src/middleware.js
+++ my-app-four/src/middleware.js
@@ -2,31 +2,33 @@ import {NextResponse} from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
+  console.log('Middleware executed')
   // your async logic here step: 1
   //  return NextResponse.redirect(new URL('/', request.url))
   // step: 2 
-  if(request.nextUrl.pathname === '/profile') {   return NextResponse.redirect(new URL('/', request.url)) } 
-  return NextResponse.next()
+  // if(request.nextUrl.pathname === '/profile') {   return NextResponse.redirect(new URL('/', request.url)) } 
+  // return NextResponse.next()
+
   // step:3 Get a cookie from the request
-  // const cookieValue = request
-  //   .cookies
-  //   .get('myCookie')
-  //   ?.value;
+  const cookieValue = request
+    .cookies
+    .get('myCookie')
+    ?.value;
 
-  // // Create a response (could be a redirect or NextResponse.next())
-  // const response = NextResponse.next();
-  // if (cookieValue != 'cookieValueHere') {
-  //   // Set a cookie in the response
-  //   response
-  //     .cookies
-  //     .set('myCookie', 'cookieValueHere', {
-  //       httpOnly: true,
-  //       path: '/',
-  //       maxAge: 60 * 60 * 24, // 1 day
-  //     });
-  // }
+  // Create a response (could be a redirect or NextResponse.next())
+  const response = NextResponse.next();
+  if (cookieValue != 'cookieValueHere') {
+    // Set a cookie in the response
+    response
+      .cookies
+      .set('myCookie', 'cookieValueHere', {
+        httpOnly: true,
+        path: '/',
+        maxAge: 60 * 60 * 24, // 1 day
+      });
+  }
 
-  // return response
+  return response
 
 }
 
